2b364bb3c70203239ac3d0389aaa9e27d85ae618
Muhammad Rafiul Ilmi Syarifudin <mr.is@arc.itb.ac.id>
2016-10-23 06:35:40 +0700

    Contributor beres

--- application/views/contributor.php
+++ application/views/contributor.php
@@ -1,123 +1,107 @@
 </header>
 <div class="container-fluid">
 <div class="row contributor" id="photos">
-		<a id="photo1" href="#sesuatu" data-toggle="modal" data-target="#showphoto">
+		<a id="photo1" href="<?= site_url()?>fotografi/profile/">
 		<div class="box col-md-3">
-			<img class="photos" src="<?= img_url()?>pp-1.jpg"/>
-			<span class="caption fade-caption">
-				<h1>Judul 1</h1>
-				<h4>Nama Seseorang</h4>
+			<img class="photos photos-contributor" src="<?= img_url()?>pp-1.jpg"/>
+			<span class="caption fade-caption photos-contributor ">
+				<h2>Nama Seseorang</h2>
 			</span>
 		</div></a>
 		<div class="box col-md-3">
-			<img class="photos" src="<?= img_url()?>pp-2.jpg"/>
-			<span class="caption fade-caption">
-				<h1>Judul 2</h1>
-				<h4>Nama Seseorang</h4>
+			<img class="photos photos-contributor" src="<?= img_url()?>pp-2.jpg"/>
+			<span class="caption fade-caption photos-contributor ">
+				<h2>Nama Seseorang</h2>
 			</span>
 		</div>
 		<div class="box col-md-3">
-			<img class="photos" src="<?= img_url()?>pp-3.jpg"/>
-			<span class="caption fade-caption">
-				<h1>Judul 4 </h1>
-				<h4>Nama Seseorang</h4>
+			<img class="photos photos-contributor" src="<?= img_url()?>pp-3.jpg"/>
+			<span class="caption fade-caption photos-contributor ">
+				<h2>Nama Seseorang</h2>
 			</span>
 		</div>
 		<div class="box col-md-3">
-			<img class="photos" src="<?= img_url()?>pp-4.jpg"/>
-			<span class="caption fade-caption">
-				<h1>Judul 5</h1>
-				<h4>Nama Seseorang</h4>
+			<img class="photos photos-contributor" src="<?= img_url()?>pp-4.jpg"/>
+			<span class="caption fade-caption photos-contributor ">
+				<h2>Nama Seseorang</h2>
 			</span>
 		</div>
 	</div>
 	<div class="row" id="photos">	
 		<div class="box col-md-3">
-			<img class="photos" src="<?= img_url()?>pp-5.jpg"/>
-			<span class="caption fade-caption">
-				<h1>Judul 4 </h1>
-				<h4>Nama Seseorang</h4>
+			<img class="photos photos-contributor" src="<?= img_url()?>pp-5.jpg"/>
+			<span class="caption fade-caption photos-contributor ">
+				<h2>Nama Seseorang</h2>
 			</span>
 		</div>
 		<div class="box col-md-3">
-			<img class="photos" src="<?= img_url()?>pp-6.jpg"/>
-			<span class="caption fade-caption">
-				<h1>Judul 5</h1>
-				<h4>Nama Seseorang</h4>
+			<img class="photos photos-contributor" src="<?= img_url()?>pp-6.jpg"/>
+			<span class="caption fade-caption photos-contributor ">
+				<h2>Nama Seseorang</h2>
 			</span>
 		</div>
 		<a href="#sesuatu"><div class="box col-md-3">
-			<img class="photos" src="<?= img_url()?>pp-7.jpg"/>
-			<span class="caption fade-caption">
-				<h1>Judul 1</h1>
-				<h4>Nama Seseorang</h4>
+			<img class="photos photos-contributor" src="<?= img_url()?>pp-7.jpg"/>
+			<span class="caption fade-caption photos-contributor ">
+				<h2>Nama Seseorang</h2>
 			</span>
 		</div></a>
 		<div class="box col-md-3">
-			<img class="photos" src="<?= img_url()?>pp-8.jpg"/>
-			<span class="caption fade-caption">
-				<h1>Judul 2</h1>
-				<h4>Nama Seseorang</h4>
+			<img class="photos photos-contributor" src="<?= img_url()?>pp-8.jpg"/>
+			<span class="caption fade-caption photos-contributor ">
+				<h2>Nama Seseorang</h2>
 			</span>
 		</div>
 	</div>
 	<div class="row" id="photos">
 		<a href="#sesuatu" ><div class="box col-md-3">
-			<img class="photos" src="<?= img_url()?>pp-9.jpg"/>
-			<span class="caption fade-caption">
-				<h1>Judul 1</h1>
-				<h4>Nama Seseorang</h4>
+			<img class="photos photos-contributor" src="<?= img_url()?>pp-9.jpg"/>
+			<span class="caption fade-caption photos-contributor ">
+				<h2>Nama Seseorang</h2>
 			</span>
 		</div></a>
 		<div class="box col-md-3">
-			<img class="photos" src="<?= img_url()?>pp-10.jpg"/>
-			<span class="caption fade-caption">
-				<h1>Judul 2</h1>
-				<h4>Nama Seseorang</h4>
+			<img class="photos photos-contributor" src="<?= img_url()?>pp-10.jpg"/>
+			<span class="caption fade-caption photos-contributor ">
+				<h2>Nama Seseorang</h2>
 			</span>
 		</div>
 		<div class="box col-md-3">
-			<img class="photos" src="<?= img_url()?>pp-11.jpg"/>
-			<span class="caption fade-caption">
-				<h1>Judul 4 </h1>
-				<h4>Nama Seseorang</h4>
+			<img class="photos photos-contributor" src="<?= img_url()?>pp-11.jpg"/>
+			<span class="caption fade-caption photos-contributor ">
+				<h2>Nama Seseorang</h2>
 			</span>
 		</div>
 		<div class="box col-md-3">
-			<img class="photos" src="<?= img_url()?>pp-12.jpg"/>
-			<span class="caption fade-caption">
-				<h1>Judul 5</h1>
-				<h4>Nama Seseorang</h4>
+			<img class="photos photos-contributor" src="<?= img_url()?>pp-12.jpg"/>
+			<span class="caption fade-caption photos-contributor ">
+				<h2>Nama Seseorang</h2>
 			</span>
 		</div>
 	</div>
 	<div class="row" id="photos">	
 		<div class="box col-md-3">
-			<img class="photos" src="<?= img_url()?>pp-4.jpg"/>
-			<span class="caption fade-caption">
-				<h1>Judul 4 </h1>
-				<h4>Nama Seseorang</h4>
+			<img class="photos photos-contributor" src="<?= img_url()?>pp-4.jpg"/>
+			<span class="caption fade-caption photos-contributor ">
+				<h2>Nama Seseorang</h2>
 			</span>
 		</div>
 		<div class="box col-md-3">
-			<img class="photos" src="<?= img_url()?>pp-5.jpg"/>
-			<span class="caption fade-caption">
-				<h1>Judul 5</h1>
-				<h4>Nama Seseorang</h4>
+			<img class="photos photos-contributor" src="<?= img_url()?>pp-5.jpg"/>
+			<span class="caption fade-caption photos-contributor ">
+				<h2>Nama Seseorang</h2>
 			</span>
 		</div>
 		<a href="#sesuatu" ><div class="box col-md-3">
-			<img class="photos" src="<?= img_url()?>pp-1.jpg"/>
-			<span class="caption fade-caption">
-				<h1>Judul 1</h1>
-				<h4>Nama Seseorang</h4>
+			<img class="photos photos-contributor" src="<?= img_url()?>pp-1.jpg"/>
+			<span class="caption fade-caption photos-contributor ">
+				<h2>Nama Seseorang</h2>
 			</span>
 		</div></a>
 		<div class="box col-md-3">
-			<img class="photos" src="<?= img_url()?>pp-2.jpg"/>
-			<span class="caption fade-caption">
-				<h1>Judul 2</h1>
-				<h4>Nama Seseorang</h4>
+			<img class="photos photos-contributor" src="<?= img_url()?>pp-2.jpg"/>
+			<span class="caption fade-caption photos-contributor ">
+				<h2>Nama Seseorang</h2>
 			</span>
 		</div>
 	</div>
