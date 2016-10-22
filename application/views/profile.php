4eeea52697114caa7a6aee0c5e3dd1a0691a849b
Muhammad Rafiul Ilmi Syarifudin <mr.is@arc.itb.ac.id>
2016-10-23 01:13:49 +0700

    contributor almost finish, only modal to show lightbox photo yet

--- application/views/profile.php
+++ application/views/profile.php
@@ -14,36 +14,20 @@
 				</div>
 			</div>
 		</div>
-		<div class="col-md-5" style="display: none;">
+		<div id="profile_photos" class="col-md-5" >
 			<div id="photos" class="profile_photos">
 				<h2>Photos</h2>
 				<div class="scroll">
 				<div class="row">
 					<div class="box col-md-6">
-						<img class="photos" src="<?= img_url()?>gambar8.jpg"/>
+						<img class="photos" src="<?= img_url()?>pp-8.jpg"/>
 						<span class="caption fade-caption">
 							<h1>Judul 2</h1>
 							<h4>Nama Seseorang</h4>
 						</span>
 					</div>
 					<div class="box col-md-6">
-						<img class="photos" src="<?= img_url()?>gambar2.jpg"/>
-						<span class="caption fade-caption">
-							<h1>Judul 2</h1>
-							<h4>Nama Seseorang</h4>
-						</span>
-					</div>
-				</div>
-				<div class="row">
-					<div class="box col-md-6">
-						<img class="photos" src="<?= img_url()?>gambar1.jpg"/>
-						<span class="caption fade-caption">
-							<h1>Judul 2</h1>
-							<h4>Nama Seseorang</h4>
-						</span>
-					</div>
-					<div class="box col-md-6">
-						<img class="photos" src="<?= img_url()?>gambar5.jpg"/>
+						<img class="photos" src="<?= img_url()?>pp-2.jpg"/>
 						<span class="caption fade-caption">
 							<h1>Judul 2</h1>
 							<h4>Nama Seseorang</h4>
@@ -52,30 +36,14 @@
 				</div>
 				<div class="row">
 					<div class="box col-md-6">
-						<img class="photos" src="<?= img_url()?>gambar8.jpg"/>
+						<img class="photos" src="<?= img_url()?>pp-1.jpg"/>
 						<span class="caption fade-caption">
 							<h1>Judul 2</h1>
 							<h4>Nama Seseorang</h4>
 						</span>
 					</div>
 					<div class="box col-md-6">
-						<img class="photos" src="<?= img_url()?>gambar2.jpg"/>
-						<span class="caption fade-caption">
-							<h1>Judul 2</h1>
-							<h4>Nama Seseorang</h4>
-						</span>
-					</div>
-				</div>
-				<div class="row">
-					<div class="box col-md-6">
-						<img class="photos" src="<?= img_url()?>gambar1.jpg"/>
-						<span class="caption fade-caption">
-							<h1>Judul 2</h1>
-							<h4>Nama Seseorang</h4>
-						</span>
-					</div>
-					<div class="box col-md-6">
-						<img class="photos" src="<?= img_url()?>gambar5.jpg"/>
+						<img class="photos" src="<?= img_url()?>pp-5.jpg"/>
 						<span class="caption fade-caption">
 							<h1>Judul 2</h1>
 							<h4>Nama Seseorang</h4>
@@ -84,14 +52,14 @@
 				</div>
 				<div class="row">
 					<div class="box col-md-6">
-						<img class="photos" src="<?= img_url()?>gambar8.jpg"/>
+						<img class="photos" src="<?= img_url()?>pp-8.jpg"/>
 						<span class="caption fade-caption">
 							<h1>Judul 2</h1>
 							<h4>Nama Seseorang</h4>
 						</span>
 					</div>
 					<div class="box col-md-6">
-						<img class="photos" src="<?= img_url()?>gambar2.jpg"/>
+						<img class="photos" src="<?= img_url()?>pp-2.jpg"/>
 						<span class="caption fade-caption">
 							<h1>Judul 2</h1>
 							<h4>Nama Seseorang</h4>
@@ -100,14 +68,46 @@
 				</div>
 				<div class="row">
 					<div class="box col-md-6">
-						<img class="photos" src="<?= img_url()?>gambar1.jpg"/>
+						<img class="photos" src="<?= img_url()?>pp-1.jpg"/>
 						<span class="caption fade-caption">
 							<h1>Judul 2</h1>
 							<h4>Nama Seseorang</h4>
 						</span>
 					</div>
 					<div class="box col-md-6">
-						<img class="photos" src="<?= img_url()?>gambar5.jpg"/>
+						<img class="photos" src="<?= img_url()?>pp-5.jpg"/>
+						<span class="caption fade-caption">
+							<h1>Judul 2</h1>
+							<h4>Nama Seseorang</h4>
+						</span>
+					</div>
+				</div>
+				<div class="row">
+					<div class="box col-md-6">
+						<img class="photos" src="<?= img_url()?>pp-8.jpg"/>
+						<span class="caption fade-caption">
+							<h1>Judul 2</h1>
+							<h4>Nama Seseorang</h4>
+						</span>
+					</div>
+					<div class="box col-md-6">
+						<img class="photos" src="<?= img_url()?>pp-2.jpg"/>
+						<span class="caption fade-caption">
+							<h1>Judul 2</h1>
+							<h4>Nama Seseorang</h4>
+						</span>
+					</div>
+				</div>
+				<div class="row">
+					<div class="box col-md-6">
+						<img class="photos" src="<?= img_url()?>pp-1.jpg"/>
+						<span class="caption fade-caption">
+							<h1>Judul 2</h1>
+							<h4>Nama Seseorang</h4>
+						</span>
+					</div>
+					<div class="box col-md-6">
+						<img class="photos" src="<?= img_url()?>pp-5.jpg"/>
 						<span class="caption fade-caption">
 							<h1>Judul 2</h1>
 							<h4>Nama Seseorang</h4>
@@ -117,26 +117,126 @@
 				</div>
 			</div>
 		</div>
-		<div class="col-md-5">
+		<div id="prev_projects" class="col-md-1" style="display: none;">
+			<a href="#">
+		      <img class="btn-right left" src="<?= img_url()?>chevron.png">
+		    </a>
+		</div>
+		<div id="profile_projects" class="col-md-5" style="display: none;">
 			<div class="profile_projects">
 				<h2>Projects</h2>
 				<div class="scroll">
-					<div class="row">
+					<div class="row	">
 						<div class="col-md-6 img-projects">
-							<img class="photos" src="<?= img_url()?>pp-1.jpg"/>
+							<img class="photos" src="<?= img_url()?>pp-3.jpg"/>
 						</div>
 						<div class="col-md-6 desc-projects">
 							<h1>Title Project</h1>
-							<p>jasdoa joasd</p>
+							<h3>2012</h3>
+							<h3>Deskripsi</h3>
+							<h3>group project</h3>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-6 img-projects">
+							<img class="photos" src="<?= img_url()?>pp-3.jpg"/>
+						</div>
+						<div class="col-md-6 desc-projects">
+							<h1>Title Project</h1>
+							<h3>2012</h3>
+							<h3>Deskripsi</h3>
+							<h3>group project</h3>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-6 img-projects">
+							<img class="photos" src="<?= img_url()?>pp-3.jpg"/>
+						</div>
+						<div class="col-md-6 desc-projects">
+							<h1>Title Project</h1>
+							<h3>2012</h3>
+							<h3>Deskripsi</h3>
+							<h3>group project</h3>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-6 img-projects">
+							<img class="photos" src="<?= img_url()?>pp-3.jpg"/>
+						</div>
+						<div class="col-md-6 desc-projects">
+							<h1>Title Project</h1>
+							<h3>2012</h3>
+							<h3>Deskripsi</h3>
+							<h3>group project</h3>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="col-md-1">
+		<div id="next_photos" class="col-md-1" >
 			<a href="#">
 		      <img class="btn-right right" src="<?= img_url()?>chevron.png">
 		    </a>
 		</div>
 	</div>
 </div>
+<!-- Modal -->
+<div id="showphoto" class="modal">
+	<div class="photo-modal-content">
+		<div class="col-mod-12">
+			<img class="photosmodal" id="modal-photo-image"/>
+		</div>
+	</div>
+	<a id="description" href="#" class="btn-modal">
+      <img class="btn-right " src="<?= img_url()?>chevron.png">
+    </a>
+</div>
+<div id="showdescphoto" class="modal">
+	<div class="photo-modal-content">
+		<div class="col-mod-12">
+			<div class="content-desc">
+				<h1 id="modal-photo-title"></h1>
+				<h2 id="modal-photo-author"></h2>
+				<p  id="modal-photo-caption"></p>
+				<br>
+				<p><span id="modal-photo-other"></span>
+				<span id="modal-photo-location"></span>
+				<span id="modal-photo-gear"></span>
+				</p>				
+			</div>
+			<a id="up-photo" href="#" class="btn-modal">
+		      <img class="btn-right up" src="<?= img_url()?>chevron.png">
+		    </a>
+		</div>
+	</div>
+</div>
+
+<script>
+(function(){
+	//Get the modal
+
+	var profile_photos = document.getElementById('profile_photos');
+	var profile_projects = document.getElementById('profile_projects');
+
+	// Get the button that opens the modal
+	var prev_projects = document.getElementById("prev_projects");
+	var next_photos = document.getElementById("next_photos");
+
+	// When the user clicks on the button, open the modal
+	next_photos.onclick = function(){
+		$('#profile_photos').css("display","none");
+		$('#profile_projects').css("display", "block");
+		$("#next_photos").modal();
+		$('#next_photos').css("display", "none");
+		$('#prev_projects').css("display", "block");
+	}
+	prev_projects.onclick = function(){
+		$('#profile_photos').css("display","block");
+		$('#profile_projects').css("display", "none");
+		$("#next_photos").modal();
+		$('#next_photos').css("display", "block");
+		$('#prev_projects').css("display", "none");
+	}
+}());
+
+</script>
