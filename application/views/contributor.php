e0e75adc840bd0684862086328e0ccc8e01e15a5
Ibrohim Kholilul Islam <ibrohimislam@gmail.com>
2016-10-23 12:51:59 +0700

    contributors, test

--- application/views/contributor.php
+++ application/views/contributor.php
@@ -1,23 +1,31 @@
 </header>
 <div class="container-fluid">
-<div class="row contributor" id="photos">
-	<div class="row" id="photos">
-<?php $i = 0; foreach ($contributors as $contributor) {
-	$i++;
-?>
-		<a href="<?= site_url()?>fotografi/profile/<?=$contributor->id?>">
+	<div class="row contributor" id="photos">
+		<a id="photo1" href="<?= site_url()?>fotografi/profile/">
 		<div class="box col-md-3">
-			<img class="photos photos-contributor" src="<?= img_url().$contributor->foto?>"/>
+			<img class="photos photos-contributor" src="<?= img_url()?>pp-1.jpg"/>
 			<span class="caption fade-caption photos-contributor ">
-				<h2><?=$contributor->first_name?></h2>
+				<h2>Nama Seseorang</h2>
+			</span>
+		</div></a>
+		<div class="box col-md-3">
+			<img class="photos photos-contributor" src="<?= img_url()?>pp-2.jpg"/>
+			<span class="caption fade-caption photos-contributor ">
+				<h2>Nama Seseorang</h2>
 			</span>
 		</div>
-		</a>
-
-<?php
-	if (($i>0) && ($i%4==0) && (($i+1)<count($contributors))) echo "</div><div class=\"row\" id=\"photos\">";
-}
-?>
- 	</div>
+		<div class="box col-md-3">
+			<img class="photos photos-contributor" src="<?= img_url()?>pp-3.jpg"/>
+			<span class="caption fade-caption photos-contributor ">
+				<h2>Nama Seseorang</h2>
+			</span>
+		</div>
+		<div class="box col-md-3">
+			<img class="photos photos-contributor" src="<?= img_url()?>pp-4.jpg"/>
+			<span class="caption fade-caption photos-contributor ">
+				<h2>Nama Seseorang</h2>
+			</span>
+		</div>
+	</div>
 </div>
 </div>
