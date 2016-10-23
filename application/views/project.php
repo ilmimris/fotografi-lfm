dff87e00d24ebf5df8e6084b0d63f42060df4370
Muhammad Rafiul Ilmi Syarifudin <mr.is@arc.itb.ac.id>
2016-10-23 09:03:36 +0700

    add page project and editprofile

--- application/views/project.php
+++ application/views/project.php
@@ -1,4 +1,11 @@
 </header>
 <div class="container-fluid">
-Project
+	<div class="row">
+		<h1><u>Project</u></h1>
+	</div>
+	<div class="row">
+		<div class="col-md-4"></div>
+		<div class="col-md-4"></div>
+		<div class="col-md-4"></div>
+	</div>
 </div>
