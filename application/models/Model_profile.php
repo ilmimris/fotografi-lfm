a0982d6579d2defe74d2bfcfa8f640241d06f1b5
Ibrohim Kholilul Islam <ibrohimislam@gmail.com>
2016-10-23 09:26:36 +0700

    merge

---
+++ application/models/Model_profile.php
@@ -0,0 +1,25 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+if (!class_exists('ORM')) require APPPATH . '/models/ORM.php';
+
+
+class Model_profile extends ORM {
+
+  public function __construct(){
+    parent::__construct();
+  }
+
+  protected $table = 'users_profile';
+
+  protected $contract = ["id", "jurusan", "angkatan", "angkatan_lfm", "email_alternatif", "foto"];
+
+  protected function _find_query($filter){
+    $this->db->select(["users_profile.*","users.first_name","users.last_name"]);
+    $this->db->join("users", "users_profile.user_id=users.id", "left");
+
+    $this->db->where($filter);
+    return $this->db->get($this->table);
+  }
+}
