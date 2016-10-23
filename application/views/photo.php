ff2734a1dcb2396e10ababe5b6206fc95de44991
Ibrohim Kholilul Islam <ibrohimislam@gmail.com>
2016-10-23 08:25:24 +0700

    photos, jquery, test

--- application/views/photo.php
+++ application/views/photo.php
@@ -133,7 +133,7 @@
 
 	// When the user clicks anywhere outside of the modal, close it
 	window.onclick = function(event) {
-	    if (event.target != showdesc) {
+	    if (event.target == showdesc) {
 	        $('#showphoto').modal('hide');
 	    }
 	}
