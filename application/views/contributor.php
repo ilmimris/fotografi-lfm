337f95f885644912d98af2b4ea807a946ddb8239
Ibrohim Kholilul Islam <ibrohimislam@gmail.com>
2016-10-23 12:50:33 +0700

    contributors, test

--- application/views/contributor.php
+++ application/views/contributor.php
@@ -7,7 +7,7 @@
 ?>
 		<a href="<?= site_url()?>fotografi/profile/<?=$contributor->id?>">
 		<div class="box col-md-3">
-			<img class="photos photos-contributor" src="<?= img_url().$contributor->photo?>"/>
+			<img class="photos photos-contributor" src="<?= img_url().$contributor->foto?>"/>
 			<span class="caption fade-caption photos-contributor ">
 				<h2><?=$contributor->first_name?></h2>
 			</span>
