fcace3e39f983d81eb2481522b5d5d77624c0748
Ibrohim Kholilul Islam <ibrohimislam@gmail.com>
2016-10-23 19:25:59 +0700

    clean up

--- application/views/header.php
+++ application/views/header.php
@@ -63,7 +63,7 @@
 	    </ul>
     </div>
     <?php if (!$islogin == 0) {?>
-    <div id="profpic"><a href="<?=site_url()?>/fotografi/edit_profile"><img class="img-circle" src="<?= img_url()?><?=$user->foto?>"></a></div>
+    <<a href="<?=site_url()?>/fotografi/edit_profile">div id="profpic"><img class="img-circle" src="<?= img_url()?><?=$user->foto?>"></div></a>
     <?php } ?>
   </div>
 </nav>
