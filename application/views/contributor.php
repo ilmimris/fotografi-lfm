04de1fa281b42041e9fe8d5033873125b139cf3e
Ibrohim Kholilul Islam <ibrohimislam@gmail.com>
2016-10-23 12:53:57 +0700

    contributors, test

--- application/views/contributor.php
+++ application/views/contributor.php
@@ -1,25 +1,22 @@
 </header>
 <div class="container-fluid">
 	<div class="row" id="photos">
+<?php $i = 0; foreach ($contributors as $contributor) {
+	$i++;
+?>
 		<a id="photo1" href="<?= site_url()?>fotografi/profile/">
 		<div class="box col-md-3">
 			<img class="photos photos-contributor" src="<?= img_url()?>pp-1.jpg"/>
 			<span class="caption fade-caption photos-contributor ">
 				<h2>Nama Seseorang</h2>
 			</span>
-		</div></a>
-		<div class="box col-md-3">
-			<img class="photos photos-contributor" src="<?= img_url()?>pp-2.jpg"/>
-			<span class="caption fade-caption photos-contributor ">
-				<h2>Nama Seseorang</h2>
-			</span>
 		</div>
-		<div class="box col-md-3">
-			<img class="photos photos-contributor" src="<?= img_url()?>pp-3.jpg"/>
-			<span class="caption fade-caption photos-contributor ">
-				<h2>Nama Seseorang</h2>
-			</span>
-		</div>
-	</div>
+		</a>
+
+<?php
+	if (($i>0) && ($i%4==0) && (($i+1)<count($photos))) echo "</div><div class=\"row\" id=\"photos\">";
+}
+?>
+ 	</div>
 </div>
 </div>
