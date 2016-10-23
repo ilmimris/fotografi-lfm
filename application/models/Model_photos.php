44c99d68febec4ed990e3d42778f890323101a1a
Ibrohim Kholilul Islam <ibrohimislam@gmail.com>
2016-10-23 08:12:27 +0700

    register, test

--- application/models/Model_photos.php
+++ application/models/Model_photos.php
@@ -21,6 +21,6 @@ class Model_photos extends ORM {
     $this->db->order_by("photos.created", "desc"); 
 
     $this->db->where($filter);
-    return $this->db->get($this->$tabel);
+    return $this->db->get($this->tabel);
   }
 }
