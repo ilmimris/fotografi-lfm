9d5cc54d5ce25ddaeee4599e5f6fde201e291f1f
Ibrohim Kholilul Islam <ibrohimislam@gmail.com>
2016-10-23 19:24:21 +0700

    clean up

--- application/controllers/Fotografi.php
+++ application/controllers/Fotografi.php
@@ -9,6 +9,7 @@ class Fotografi extends CI_Controller {
 		$this->load->database();
 		$this->load->library(array('ion_auth','form_validation'));
 		$this->load->helper(array('url','language'));
+		$this->load->model("model_profile");
 
 		$this->form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'ion_auth'), $this->config->item('error_end_delimiter', 'ion_auth'));
 
