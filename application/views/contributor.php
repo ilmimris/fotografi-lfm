d65e0ce70a03434a4ccec7fd3cc43a60fc7ab21e
Muhammad Rafiul Ilmi Syarifudin <mr.is@arc.itb.ac.id>
2016-10-30 11:29:10 +0700

    revisi beberapa

--- application/views/contributor.php
+++ application/views/contributor.php
@@ -6,12 +6,7 @@
 ?>
 		<a id="photo1" href="<?= site_url()?>fotografi/profile/<?=$contributor->id?>">
 		<div class="box col-md-3">
-		<?php if (isset($contributor->foto)) {?>
 			<img class="photos photos-contributor" src="<?= img_url()."users_profile/_thumb/".$contributor->foto?>"/>
-		<?php }
-			else {?>
-			<img class="photos photos-contributor" src="<?= img_url()?>dummy.jpg"/>
-		<?php }?>
 			<span class="caption fade-caption photos-contributor">
 				<h2><?=$contributor->first_name?> <?=$contributor->last_name?></h2>
 			</span>
@@ -19,7 +14,7 @@
 		</a>
 
 <?php
-	if (($i>0) && ($i%4==0) && (($i+1)<count($photos))) echo "</div><div class=\"row\" id=\"photos\">";
+	if (($i>0) && ($i%4==0) && isset($photos) && (($i+1)<count($photos))) echo "</div><div class=\"row\" id=\"photos\">";
 }
 ?>
  	</div>
