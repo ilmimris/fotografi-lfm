48208b75c7c37aa973eb6d433930aa0111d71dca
Muhammad Rafiul Ilmi Syarifudin <mr.is@arc.itb.ac.id>
2016-10-30 11:22:01 +0700

    revisi beberapa

--- application/views/contributor.php
+++ application/views/contributor.php
@@ -6,7 +6,12 @@
 ?>
 		<a id="photo1" href="<?= site_url()?>fotografi/profile/<?=$contributor->id?>">
 		<div class="box col-md-3">
+		<?php if isset($contributor->foto) {?>
 			<img class="photos photos-contributor" src="<?= img_url()."users_profile/_thumb/".$contributor->foto?>"/>
+		<?php }
+			else {?>
+			<img class="photos photos-contributor" src="<?= img_url()?>dummy.jpg"/>
+		<?php }?>
 			<span class="caption fade-caption photos-contributor">
 				<h2><?=$contributor->first_name?> <?=$contributor->last_name?></h2>
 			</span>
