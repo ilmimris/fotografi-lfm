da226a978e7077dc2615e170d3d8de29af01a2f4
Muhammad Rafiul Ilmi Syarifudin <mr.is@arc.itb.ac.id>
2016-11-14 01:27:07 +0700

    pom

--- application/controllers/Fotografi.php
+++ application/controllers/Fotografi.php
@@ -35,7 +35,7 @@ class Fotografi extends CI_Controller {
 		 		'gambar2' => $gambar2->photo,
 		 		'gambar3' => $gambar3->photo
 			);
-		$data['gambar'] = $gambar[0];
+		$data['gambar'] = $gambar[0]->$result();
 		$data['imgs'] = $pom;
 		// var_dump($pom[0]->photo);
 		// var_dump($pom);
