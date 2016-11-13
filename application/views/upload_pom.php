e6974da971e2067ed6827c891d35355b661ebc7c
Muhammad Rafiul Ilmi Syarifudin <mr.is@arc.itb.ac.id>
2016-11-14 01:44:22 +0700

    pom

--- application/views/upload_pom.php
+++ application/views/upload_pom.php
@@ -123,8 +123,8 @@
         formData.append('bulan', $('#pom-month').val());
 
         $("#input-pom-form input[type=hidden]").each(function(i, obj) {
-            formData.append($(obj).attr('name'), $(obj).val(photo));
-            console.log($(obj).val(photo));
+            formData.append($(obj).attr('name'), $(obj).val());
+            console.log($(obj).val());
         });
 
         // $('#input-pom-progress').css("display","block");
