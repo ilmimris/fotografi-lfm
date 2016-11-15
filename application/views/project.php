55320331288a4373ceb45481dcd600b87fd32ae7
Muhammad Rafiul Ilmi Syarifudin <mr.is@arc.itb.ac.id>
2016-11-15 19:05:52 +0700

    edit_profile repair

--- application/views/project.php
+++ application/views/project.php
@@ -12,7 +12,7 @@
 			<div class="col-md-4">
 				<img class="photos" src="<?= img_url()?>users_content/<?=$project->photo?>"/>
 				<span class="project-caption">
-					<h3><?=$project->title?></h3>
+					<h3 style="font-family: 'Daun';"><?=$project->title?></h3>
 				</span>
 			</div>
 			</a>
