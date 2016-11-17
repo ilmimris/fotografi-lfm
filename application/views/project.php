0c8675cd99bc7724cb7b7b7e11a3137d161bb622
Muhammad Rafiul Ilmi Syarifudin <mr.is@arc.itb.ac.id>
2016-11-17 21:15:08 +0700

    project

--- application/views/project.php
+++ application/views/project.php
@@ -6,8 +6,8 @@
 	$i++;
 ?>
 			<a href="#" onclick="open_detail.call(this)" data-id="<?=$project->id?>" class="show-detail-project">
-			<div class="col-md-4 project">
-				<img class="photos" src="<?= img_url()?>users_content/<?=$project->photo?>"/>
+			<div class="col-md-4">
+				<div class="project" style="background-image: url('<?= img_url()?>users_content/<?=$project->photo?>')"></div>
 				<span class="project-caption">
 					<h3 style="font-family: 'Daun'; font-size: 2.3em; text-transform: uppercase;"><?=$project->title?></h3>
 				</span>
