993d2579862fd799cad7760453d659c862e65d24
Muhammad Rafiul Ilmi Syarifudin <mr.is@arc.itb.ac.id>
2016-11-17 17:21:01 +0700

    project

--- application/views/project.php
+++ application/views/project.php
@@ -22,7 +22,7 @@
 			</div>
 		</a>
 <?php };};
-	echo {"mod i=".$i%}if (($i%3==0)) {echo "</div>mod i</div><div class=\"row\""};
+	if (($i%3==0)) {echo "</div>mod i</div><div class=\"row\""};
 }
 ?>
 		</div>
