33262ee764e3c2c0ef0b5a7ebd97ea70532c1217
Muhammad Rafiul Ilmi Syarifudin <mr.is@arc.itb.ac.id>
2016-11-17 16:21:42 +0700

    project

--- application/views/project.php
+++ application/views/project.php
@@ -1,21 +1,23 @@
 </header>
 <div class="container-fluid">
 	<div class="show-project">
-		<div class="row" >
 <?php $i = 0; foreach ($projects as $project) {
 	$i++;
 ?>
-			<a href="#" onclick="open_detail.call(this)" data-id="<?=$project->id?>" class="show-detail-project">
+		<a href="#" onclick="open_detail.call(this)" data-id="<?=$project->id?>" class="show-detail-project">
+		<div class="row" >
 			<div class="col-md-4 project" style="background-image: url('<?= img_url()?>users_content/<?=$project->photo?>');">
 			</div>
+		</div>
+		<div class="row">
 			<div class="col-md-4">
 				<span class="project-caption">
 					<h3 style="font-family: 'Daun'; font-size: 2.3em; text-transform: uppercase;"><?=$project->title?></h3>
 				</span>
 			</div>
-			</a>
+		</div>
 <?php
-	if (($i>0) && ($i%3==0) && (($i+1)<count($projects))) echo "</div><div class=\"row\"";
+	if (($i>0) && ($i%3==0) && (($i+1)<count($projects))) echo "</a>";
 }
 ?>
 		</div>
