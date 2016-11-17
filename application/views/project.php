35fbab01f6744f5922187c256fcefbef27e61e4c
Muhammad Rafiul Ilmi Syarifudin <mr.is@arc.itb.ac.id>
2016-11-17 17:34:06 +0700

    project

--- application/views/project.php
+++ application/views/project.php
@@ -14,8 +14,10 @@
 <?php 
 	if (($i%3==0)) {
 		echo "</div><div class=\"row\""; ?>
-<?php  
+<?php
+		$k=0;  
 		foreach ($projects as $project) { 
+			$k++;
 ?>
 			<a href="#" onclick="open_detail.call(this)" data-id="<?=$project->id?>" class="show-detail-project">
 				<div class="col-md-4" style="width: 300px; margin: auto 3.33333%;">
@@ -25,7 +27,7 @@
 				</div>
 			</a>
 <?php 
-		if (($i%3==0)) { echo "</div><div class=\"row\"";}
+		if (($k%3==0)) { echo "</div><div class=\"row\"";}
 		};
 	};
 	};	
