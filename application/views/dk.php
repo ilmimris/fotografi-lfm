18c8bff7bd0326201b4b2cd65f23326169874267
Muhammad Rafiul Ilmi Syarifudin <mr.is@arc.itb.ac.id>
2016-11-28 06:38:17 +0700

    project

--- application/views/dk.php
+++ application/views/dk.php
@@ -6,7 +6,7 @@
 <?php $i = 0; foreach ($dks as $dk) {
 	$i++;
 ?>
-				<div class="inline img-dk"><a href="#" onclick="open_detail_dk.call(this)" class="btn-dk-desc" style="text-decoration: none;" data-id="<?=$dk->id?>"><img style="height: 410px;width: 230px;" src="<?= img_url().'users_content/'.$dk->cover?>"><h4 class="dk-month"><?= $dk->month ?></h4><h4 class="dk-title"><?= $dk->title ?></h4></a></div>
+				<div class="inline img-dk" style="max-width: 231px;"><a href="#" onclick="open_detail_dk.call(this)" class="btn-dk-desc" style="text-decoration: none;" data-id="<?=$dk->id?>"><img style="height: 410px;width: 230px;" src="<?= img_url().'users_content/'.$dk->cover?>"><h4 class="dk-month"><?= $dk->month ?></h4><h4 class="dk-title"><?= $dk->title ?></h4></a></div>
 <?php }?>
 			</div>
 		</div>
