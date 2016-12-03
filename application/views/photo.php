579377a682e12a782c84b99dffe543e8ab409d62
Muhammad Rafiul Ilmi Syarifudin <mr.is@arc.itb.ac.id>
2016-12-03 18:31:09 +0700

    project

--- application/views/photo.php
+++ application/views/photo.php
@@ -17,7 +17,7 @@
 			<img class="photos" src="<?=img_url()."users_content/_thumb/".$photo->photo?>"/>
 			<span class="caption fade-caption">
 				<h1><?=$photo->title?></h1>
-				<h4><?=$photo->author?></h4>
+				<a href="#nama"><h4><?=$photo->author?></h4></a>
 			</span>
 		</div>
 		</a>
