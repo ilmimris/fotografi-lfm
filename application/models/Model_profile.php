4a9d3d51c109e83f0aab69f72b692507f52b91e4
Muhammad Rafiul Ilmi Syarifudin <mr.is@arc.itb.ac.id>
2016-12-04 06:21:07 +0700

    project

--- application/models/Model_profile.php
+++ application/models/Model_profile.php
@@ -18,7 +18,7 @@ class Model_profile extends ORM {
   protected function _find_query($filter){
     $this->db->select(["users_profile.*","users.first_name","users.last_name","users.email","users.username"]);
     $this->db->join("users", "users_profile.id=users.id", "left");
-    $this->db->order_by("users.created_on","asc");
+    $this->db->order_by("users.created_on","desc");
     $this->db->where($filter);
     return $this->db->get($this->table);
   }
